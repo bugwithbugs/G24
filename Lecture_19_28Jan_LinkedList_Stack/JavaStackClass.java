46897e536239531bc34dec4958979c827379fd4a
Vaibhav <vkpgpay@gmail.com>
2025-01-28 23:24:25 +0530

    Linkedlist & stack

---
+++ Lecture_19_28Jan_LinkedList_Stack/JavaStackClass.java
@@ -0,0 +1,23 @@
+package Lecture_19_28Jan_LinkedList_Stack;
+
+import java.util.Stack;
+
+public class JavaStackClass {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        st.push(40);
+
+        System.out.println("Current size of stack: " + st.size());
+
+        System.out.println("popping: " + st.pop());
+
+        System.out.println("Looking at top without deleting: " + st.peek());
+
+        System.out.println("Current size of stack: " + st.size());
+
+    }
+}
